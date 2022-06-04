e767b0a1df199615b6bd49a0ae1b2418a247ca29
d0kur0 <dev.dokuro@gmail.com>
2022-06-04 22:28:17 +0300

    chore: initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+const EVENTS_PREFIX = "MF-CQRS_";
+
+export function handleEvent<PropsType>(
+	eventName: string,
+	callback: (props: PropsType) => void
+) {
+	return window.addEventListener(`${EVENTS_PREFIX}${eventName}`, ((
+		event: CustomEvent<PropsType>
+	) => callback(event.detail)) as EventListener);
+}
+
+export function invokeEvent(eventName: string, props: Object) {
+	const event = new CustomEvent(`${EVENTS_PREFIX}${eventName}`, {
+		detail: props,
+	});
+
+	window.dispatchEvent(event);
+}
